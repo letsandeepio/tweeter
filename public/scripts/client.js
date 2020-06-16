03f0d1fe0447494b2144ecc2833b67e7700c9bd2
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-16 21:24:56 +0000

    added color in tweet character count

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,12 +5,3 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-let TWEET_LIMIT = 140;
-
-$(document).ready(function () {
-  console.log('JQuery');
-  $('#tweet-text').on('keyup', function (event) {
-    $('#js--tweet-counter').text(TWEET_LIMIT - $('#tweet-text').val().length);
-  });
-});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,16 @@
-let TWEET_LIMIT = 140;
+const TWEET_LIMIT = 140;
 
 $(document).ready(function () {
   console.log('JQuery');
   $('#tweet-text').on('keyup', function (event) {
-    $('#js--tweet-counter').text(TWEET_LIMIT - this.val().length);
+    let charRemaining = TWEET_LIMIT - $(this).val().length;
+    const tweetCounter = $('#js--tweet-counter');
+
+    if (charRemaining < 0) {
+      tweetCounter.addClass('over-limit-red');
+    } else {
+      tweetCounter.removeClass('over-limit-red');
+    }
+    tweetCounter.text(charRemaining);
   });
 });
