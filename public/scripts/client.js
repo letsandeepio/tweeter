fa0518996b14f169992a1cb4355460c59b1b26f4
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-16 21:03:21 +0000

    implemented character count

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,16 @@
+/* global $ */
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+let TWEET_LIMIT = 140;
+
 $(document).ready(function () {
   console.log('JQuery');
-  $('#tweet-text').on('keypress', function (event) {
-    console.log(event);
+  $('#tweet-text').on('keyup', function (event) {
+    $('#js--tweet-counter').text(TWEET_LIMIT - $('#tweet-text').val().length);
   });
 });
