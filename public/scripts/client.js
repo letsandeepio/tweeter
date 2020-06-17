8135bd6d74db68ab769c480b584d84bf80a81d41
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-17 00:13:10 +0000

    :hammer: fixed tweet container css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,3 +5,43 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  user: {
+    name: 'Newton',
+    avatars: 'https://i.imgur.com/73hZDYK.png',
+    handle: '@SirIsaac'
+  },
+  content: {
+    text: 'If I have seen further it is by standing on the shoulders of giants'
+  },
+  created_at: 1461116232227
+};
+
+const createTweetElement = (tweetObj) => {
+  return `<article class='tweet'>
+        <header>
+          <div class="profile"><img src='images/034-lord-ram.png' class='tweet-profile-pic'></div>
+          <div class="username">${tweetObj.user.name}</div>
+          <div class="userid">@SirIsaac</div>
+        </header>
+        <main>
+          I have seen further by standing on the shoulder of giants
+        </main>
+        <footer>
+          <div class="timestamp">10 days ago</div>
+          <div class="tweet-actions">
+            <div><i class="far fa-flag"></i></div>
+            <div><i class="fas fa-retweet"></i></div>
+            <div><i class="far fa-heart"></i></div>
+          </div>
+        </footer>
+      </article>`;
+};
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
