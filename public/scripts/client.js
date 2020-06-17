ffbd525f0ab896383aa638ba74df63ae4f36248e
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-17 00:40:12 +0000

    :sparkles: added data render js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,17 +7,32 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  user: {
-    name: 'Newton',
-    avatars: 'https://i.imgur.com/73hZDYK.png',
-    handle: '@SirIsaac'
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac'
+    },
+    content: {
+      text:
+        'If I have seen further it is by standing on the shoulders of giants'
+    },
+    created_at: 1461116232227
   },
-  content: {
-    text: 'If I have seen further it is by standing on the shoulders of giants'
-  },
-  created_at: 1461116232227
-};
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd'
+    },
+    content: {
+      text: 'Je pense , donc je suis'
+    },
+    created_at: 1461113959088
+  }
+];
 
 const createTweetElement = (tweetObj) => {
   return `<article class='tweet'>
@@ -40,9 +55,10 @@ const createTweetElement = (tweetObj) => {
       </article>`;
 };
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function (tweets) {
+  for (const tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+};
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
