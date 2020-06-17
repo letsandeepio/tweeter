2b1eabeb567e780c4caa503b5038e9040b68e22b
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-17 01:29:02 +0000

    :hammer: removed hard coded tweet data obj

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,31 +10,6 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac'
-    },
-    content: {
-      text:
-        'If I have seen further it is by standing on the shoulders of giants'
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd'
-    },
-    content: {
-      text: 'Je pense , donc je suis'
-    },
-    created_at: 1461113959088
-  }
-];
 
 const createTweetElement = (tweetObj) => {
   return `<article class='tweet'>
@@ -63,8 +38,6 @@ const renderTweets = function (tweets) {
   }
 };
 
-renderTweets(data);
-
 $('#js--new-tweet').submit(function (event) {
   event.preventDefault();
   $.ajax('/tweets', { method: 'POST', data: $(this).serialize() }).then(
@@ -76,7 +49,7 @@ $('#js--new-tweet').submit(function (event) {
 
 const loadtweets = () => {
   $.ajax('/tweets', { method: 'GET' }).then(function (tweetsData) {
-    console.log('Success: ', tweetsData);
+    renderTweets(tweetsData);
   });
 };
 
